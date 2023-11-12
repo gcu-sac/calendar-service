04298dee9baadffe8ff2951a971d7f3ccc5b5b62
Yany Choi <dydghks0818@naver.com>
2023-11-12 11:32:03 +0900

    update endpoints

--- src/main/java/calendar/calendar_service/controller/ScheduleController.java
+++ src/main/java/calendar/calendar_service/controller/ScheduleController.java
@@ -3,9 +3,6 @@ package calendar.calendar_service.controller;
 import calendar.calendar_service.domain.Schedule;
 import calendar.calendar_service.domain.SendSchedule;
 import calendar.calendar_service.service.ScheduleService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.swagger.v3.core.util.Json;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,24 +18,24 @@ public class ScheduleController {
         this.scheduleService = scheduleService;
     }
 
-    @GetMapping("/")
+    @GetMapping("/event")
     public List<SendSchedule> Schedules(@RequestParam String month, @RequestParam String year) {
         System.out.println(month + "month" + year + "year");
         return scheduleService.findSchedulesByMonthAndYear(month, year);
     }
 
-    @PostMapping("/new")
+    @PostMapping("/event")
     public ResponseEntity<String> newewSchedule(@RequestBody Schedule schedule) {
         String result = scheduleService.newSchedule(schedule);
         return ResponseEntity.ok(result); // You might want to adjust the response status based on the result
     }
 
-    @PutMapping("/modify")
+    @PutMapping("/event")
     public ResponseEntity<String> modifySchedule(@RequestBody Schedule schedule) {
         String result = scheduleService.modifySchedule(schedule);
         return ResponseEntity.ok(result); // You might want to adjust the response status based on the result
     }
-    @DeleteMapping("/delete/{scheduleID}")
+    @DeleteMapping("/event/{scheduleID}")
     public ResponseEntity<String> deleteSchedule(@PathVariable int scheduleID) {
         String result = scheduleService.deleteSchedule(scheduleID);
         return ResponseEntity.ok(result); // You might want to adjust the response status based on the result
